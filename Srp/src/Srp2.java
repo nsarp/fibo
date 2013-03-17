184e6f8b1ee0a6c7a4c13747b684b2281d290d40
nsarp <heinrich.melwasul@gmail.com>
2013-03-17 18:31:58 +0200

    Update Srp2.java

--- Srp/src/Srp2.java
+++ Srp/src/Srp2.java
@@ -1,10 +1,61 @@
 
-public class Srp2 {
+public class QueueviaLinkedList extends LinkedList {
 
-	public static void main (String args []){
-	System.out.println("hello w");
-	
+	LinkedList L=new LinkedList();
+	int tail=0;
+	public QueueviaLinkedList()
+	{
+		this.tail=0;
+		L.head=null;
 	}
-	
+	public void enqueue(QueueviaLinkedList a,int i)
+	{
+		Node inserted=new Node(i);
+		tail++;
+		if(a.head==null)
+		{
+			a.head=inserted;
+		}
+		else 
+		{
+			inserted.next=a.head;
+			a.head.prev=inserted;
+			a.head=inserted;
+			inserted.prev=null;
+			
+		}
+	}
+	public void dequeue(QueueviaLinkedList a)
+	{
+		Node tmp=a.head;
+		if(a.tail==0)
+		{
+			System.out.println("Your queue is empty");
+		}
+		else
+		{
+			while(tmp!=null)
+			{
+				tmp=tmp.next;
+			}
+			tmp=a.head;
+			for(int i=0;i<tail-1;i++)
+			{
+				tmp=tmp.next;
+			}
+			tail--;
+			System.out.println(tmp.key);
+			
+		}
+	}
+	public static void main(String [] args)
+	{
+		QueueviaLinkedList a=new QueueviaLinkedList();
+		a.enqueue(a, 5);
+		a.enqueue(a, 10);
+		a.dequeue(a);
+		a.dequeue(a);
+		a.dequeue(a);
+	}
 }
 
